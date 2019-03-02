a3e9ec31d9d9d05135c0f9ddba471f71f5d993e1
shibin <shibintv@hotmail.com>
2019-03-02 14:10:36 -0600

    additional functionalities

--- server/impl.js
+++ server/impl.js
@@ -1,77 +1,135 @@
 
 const https = require('https');
-exports.add =  ()=>{
-    
-    let time =  Date.now();
+const fs = require('fs');
+const process = require('process');
+exports.add = () => {
+
+    let time = Date.now();
     var response = response_builder(time);
     return response;
 }
 
-exports.request = async ()=>{
-    
-    
+exports.request = async () => {
+
+
     var id = randomInt();
     var base_url = 'https://jsonplaceholder.typicode.com/todos/';
     var full_url = base_url + id;
     let response = await get_request(full_url);
-    
+
     return null;
-    
-    
+
+
 }
 
-exports.matrix_mul = (n)=>{
+exports.matrix_mul = (n) => {
     // Initialize random matrix without zeros
     var mat = initialize_random_mat(n);
 
     // Perform multiplication with itself
     var res = [];
-    for(var i=0;i<n;i++){
+    for (var i = 0; i < n; i++) {
         res[i] = []
-        for(var j=0;j<n;j++){
+        for (var j = 0; j < n; j++) {
             curres = 0
-            for(var k=0;k<n;k++){
+            for (var k = 0; k < n; k++) {
                 curres = curres + (mat[i][k] * mat[k][j]);
             }
-             res[i][j] = curres;    
+            res[i][j] = curres;
         }
     }
     return res
 
 }
 
+var write_file = (file_name, num_lines = 10000) => {
 
-var get_request = (url)=>{
-    
-    return new Promise((resolve,reject)=>{
-        https.get(url,(resp)=>{
-            resp.on('data',(d)=>resolve(d));
-            resp.on('error',(e)=>reject(e));
+    var content = "";
+    var time = new Date().getTime();
+    // create random file content
+    for (var i = 0; i < num_lines; i++) {
+        var content = content + i + "hello baby. I love you" + time + "\n";
+    }
+
+    // make synchronized by blocking.
+    fs.writeFileSync(file_name, content);
+}
+
+exports.io = async () => {
+
+    var f_index = randomInt();
+    var file_name = 'file_' + f_index + '.txt';
+    write_file(file_name);
+    //dummy return for the purpose of the unification.
+    return null;
+
+}
+exports.write_thread_count = async () => {
+    var file_name = 'thread_index.txt';
+    var data = 0;
+    try {
+        if (fs.existsSync(file_name)) {
+            data = fs.readFileSync(file_name);
+        }
+    }
+    catch (err) {
+
+    }
+    data = parseInt(data) + 1;
+    fs.writeFileSync(file_name, data);
+
+    return data;
+}
+exports.memory_usage = async () => {
+
+    var memory = process.memoryUsage();
+    return memory.rss;
+}
+
+var get_request = (url) => {
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (resp) => {
+            resp.on('data', (d) => resolve(d));
+            resp.on('error', (e) => reject(e));
         })
     }
     );
 }
-var randomInt = ()=>{
+var randomInt = () => {
     const max = 1000;
-    return Math.floor(Math.random()*max);
+    return Math.floor(Math.random() * max);
 }
-var write_rsponse=(callback)=>{
-    
-    let time =  Date.now();
+var write_rsponse = (callback) => {
+
+    let time = Date.now();
     var respone = response_builder(time);
-    callback(null,respone);
+    callback(null, respone);
 }
-var response_builder = (str)=>{
-    
+var response_builder = (str) => {
+
     var rsp = {};
     rsp.q_time = str;
     return JSON.stringify(rsp);
 }
+var response_builder_new = (start_time, end_time, additional_info) => {
+
+    if (!additional_info) {
+        additional_info = -1;
+    }
+    var rsp = {};
+    rsp.server_start_time = start_time;
+    rsp.server_end_time = end_time;
+    rsp.additional_info = additional_info;
+    return JSON.stringify(rsp);
+}
 exports.response_builder = response_builder;
 
-var initialize_random_mat = (n)=>{
+exports.response_builder_new = response_builder_new;
+
+var initialize_random_mat = (n) => {
     var mat = [];
-    for (var i = 0 ; i < n; i++) {
+    for (var i = 0; i < n; i++) {
         mat[i] = [];
         for (var j = 0; j < n; j++) {
             mat[i][j] = (Math.random() * (Number.MAX_VALUE - 1) + 1.0);
--- server/index.js
+++ server/index.js
@@ -1,24 +1,48 @@
 const impl = require('./impl');
 const provider = require('./provider');
-const providerType = 'GCP';
+const providerType = 'Test';
 
 exports.handler = async (event,context,callback) => {
     // TODO implement
     
     // Current UTC time
-    var time = new Date().getTime();
+    
     var cloudHandler = provider.getProvider(providerType,event,context,callback);
     var query = cloudHandler.getQueryParam("name");
-    var response = null;
+    var start_time = new Date().getTime();
+    //var q_time_response = impl.response_builder(time);
+    var response=null; // reponse do a dummy reading. You can check the response incase of debugging required.
+    var additional_info = null;
     switch(query){
         
         case "add":
-            //response = await impl.add();
-            response = String(time)
+            response = await impl.add();
             break;
         case "request":
             response = await impl.request();
+            break;
+        case "matmul":
+            response = await impl.matrix_mul(100);
+            break;
+        case "io":
+            response = await impl.io();
+            break;
+        case "memory":
+            // do mat mul first
+            response = await impl.matrix_mul(200);
+            additional_info = await impl.memory_usage();
+            break;
+        case "container":
+            additional_info = await impl.write_thread_count();
+            break;// chumma :).
     }
+    var end_time = new Date().getTime();
+    /*
+        New response.
+        Format is {server_start_time:,server_end_time:}
+    */
+    response = impl.response_builder_new(start_time,end_time,additional_info);
+
 
     // write the response
     cloudHandler.writeResponse(response);
